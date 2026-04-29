591fa3fa7bc64f777f90a611adb6ea6aa2acb16d
MKJ-One <hanleejss012@naver.com>
2026-04-29 19:09:13 +0900

    fix : HeaderUserDetails get 메서드 삭제.

--- src/main/java/com/finlearn/common/security/HeaderUserDetails.java
+++ src/main/java/com/finlearn/common/security/HeaderUserDetails.java
@@ -30,8 +30,4 @@ public class HeaderUserDetails implements UserDetails {
 
     @Override public String getPassword() { return null; }
     @Override public String getUsername() { return email; }
-    @Override public boolean isAccountNonExpired() { return true; }
-    @Override public boolean isAccountNonLocked() { return true; }
-    @Override public boolean isCredentialsNonExpired() { return true; }
-    @Override public boolean isEnabled() { return true; }
 }
